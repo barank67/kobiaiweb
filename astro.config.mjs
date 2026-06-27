02b5e0d21fb0fd66094a2543528ecd978df15740
Baran Kaya <barank67@gmail.com>
2026-06-27 11:32:54 +0300

    Prepare site for production domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://test.perda.com.tr",
+  site: "https://kobiai.com.tr",
   integrations: [sitemap()],
   output: "static"
 });
