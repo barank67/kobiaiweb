d0080967286f30594c9677f44754c109d6c52a9d
Baran Kaya <barank67@gmail.com>
2026-06-27 23:40:48 +0300

    Inline project stylesheets

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,8 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: "https://kobiai.com.tr",
   integrations: [sitemap()],
+  build: {
+    inlineStylesheets: "always"
+  },
   output: "static"
 });
